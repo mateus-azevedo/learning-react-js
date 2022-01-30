bb7e28955956d40924a31e6be633df9da1295ded
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-30 11:35:12 -0300

    uso do construtor na classe

--- js_poo/index.js
+++ js_poo/index.js
@@ -1,24 +1,13 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88833322209);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88833322209;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo.depositar(500);
-
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2);
 
 contaCorrenteRicardo.transferir(200, conta2);
 
-console.log(conta2.cliente);
-// console.log(contaCorrenteRicardo);
+console.log(contaCorrenteRicardo);
