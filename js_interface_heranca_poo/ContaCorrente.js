b9b0a4cb4e82ecf040a96e19f434bcb55a9e6054
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-30 23:05:23 -0300

    utilizando heranca em js

--- js_interface_heranca_poo/ContaCorrente.js
+++ js_interface_heranca_poo/ContaCorrente.js
@@ -1,42 +1,10 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
-  constructor(agencia, cliente) {
-    this.agencia = agencia;
-    this.cliente = cliente;
-    this._saldo = 0;
-    ContaCorrente.numeroDeContas += 1;
-  }
-
+export class ContaCorrente extends Conta {
   static numeroDeContas = 0;
 
-  set cliente(novoValor) {
-    if (novoValor instanceof Cliente) this._cliente = novoValor;
-  }
-
-  get cliente() {
-    return this._cliente;
-  }
-
-  get saldo() {
-    return this._saldo;
-  }
-
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor > 0) {
-      this._saldo += valor;
-    }
-  }
-
-  transferir(valor, conta) {
-    const valorSacado = this.sacar(valor);
-    conta.depositar(valorSacado);
+  constructor(cliente, agencia) {
+    super(0, cliente, agencia);
+    ContaCorrente.numeroDeContas += 1;
   }
 }
