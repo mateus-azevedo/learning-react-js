3fa5b15a84d00a2e0d0ad7d1efc9dbf93c8d87f7
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-31 09:54:07 -0300

    uso do polimorfismo

--- js_interface_heranca_poo/index.js
+++ js_interface_heranca_poo/index.js
@@ -1,16 +1,13 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor } from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456789");
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+const gerente = new Gerente("Ricardo", 5000, 12379845601);
+gerente.cadastrarSenha("12345");
 
-// console.log(contaCorrenteRicardo);
-// console.log(contaPoupanca);
-console.log(contaSalario);
+const estaLogado = SistemaAutenticacao.login(gerente, "12345");
+
+console.log(estaLogado);
