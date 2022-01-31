c798d6e6f3fd6d9198dc278c42e0b46d91ac8a59
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-31 10:19:05 -0300

    utilzando interfaces

--- js_interface_heranca_poo/index.js
+++ js_interface_heranca_poo/index.js
@@ -1,13 +1,17 @@
+import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionarios/Gerente.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
-diretor.cadastrarSenha("123456789");
+diretor.cadastrarSenha("123456");
 
 const gerente = new Gerente("Ricardo", 5000, 12379845601);
-gerente.cadastrarSenha("12345");
+gerente.cadastrarSenha("123");
 
-const estaLogado = SistemaAutenticacao.login(gerente, "12345");
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-console.log(estaLogado);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
