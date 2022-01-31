53ee4933578e3e5ef035d4b581f31569659757c3
Mateus Santos <m.santos.azeve@gmail.com>
2022-01-31 09:09:08 -0300

    propriedade constructor

--- js_interface_heranca_poo/index.js
+++ js_interface_heranca_poo/index.js
@@ -1,14 +1,14 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const conta = new Conta(0, cliente1, 1001);
 
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+// console.log(contaCorrenteRicardo);
+// console.log(contaPoupanca);
+console.log(conta);
