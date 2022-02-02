01c0b183496c2454513ef47acc585c5b84ff49e5
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-02 15:32:53 -0300

    renderizacao condicional

--- react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 import DadosEntregas from "./DadosEntregas";
+import { Typography } from "@material-ui/core";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais />
-      <DadosUsuarios />
-      <DadosEntregas />
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  const formularioAtual = (etapa) => {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuarios />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntregas />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  };
+
+  return <>{formularioAtual(etapaAtual)}</>;
 }
