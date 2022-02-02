2f5ce86cdae46665f9b27a056fe4afedf01e1ea4
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-02 16:21:40 -0300

    renderizacao condicional com array

--- react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,27 +2,18 @@ import React, { useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 import DadosEntregas from "./DadosEntregas";
-import { Typography } from "@material-ui/core";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const formularios = [
+    <DadosUsuarios aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntregas aoEnviar={aoEnviar} />,
+  ];
   const [etapaAtual, setEtapaAtual] = useState(0);
 
-  const proximo = () => {
+  function proximo() {
     setEtapaAtual(etapaAtual + 1);
-  };
+  }
 
-  const formularioAtual = (etapa) => {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuarios aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-      case 2:
-        return <DadosEntregas aoEnviar={aoEnviar} />;
-      default:
-        return <Typography>Erro ao selecionar formulário</Typography>;
-    }
-  };
-
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formularios[etapaAtual]}</>;
 }
