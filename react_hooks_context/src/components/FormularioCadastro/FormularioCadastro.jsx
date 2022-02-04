5757e321447ddac3c6c18e5e50d07bfcb16baa21
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-04 11:22:54 -0300

    finalizacao do formulario e o uso do stepper

--- react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 import DadosEntregas from "./DadosEntregas";
+import { Step, StepLabel, Stepper, Typography } from "@material-ui/core";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDadosColetados] = useState({});
 
   useEffect(() => {
-    console.log(dadosColetados);
+    if (etapaAtual === formularios.length - 1) aoEnviar(dadosColetados);
   });
 
   const formularios = [
     <DadosUsuarios aoEnviar={coletarDados} />,
-    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
     <DadosEntregas aoEnviar={coletarDados} />,
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>,
   ];
 
   function coletarDados(dados) {
@@ -27,5 +29,23 @@ export default function FormularioCadastro({ aoEnviar, validarCPF }) {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  return <>{formularios[etapaAtual]}</>;
+  return (
+    <>
+      <Stepper activeStep={etapaAtual}>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
+      </Stepper>
+      {formularios[etapaAtual]}
+    </>
+  );
 }
