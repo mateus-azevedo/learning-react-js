1490bd3b9c9732855951514cc836f8baaad016ee
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-04 10:55:00 -0300

    uso useEffect

--- react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react_hooks_context/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 import DadosEntregas from "./DadosEntregas";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
+  const [dadosColetados, setDadosColetados] = useState({});
+
+  useEffect(() => {
+    console.log(dadosColetados);
+  });
 
   const formularios = [
-    <DadosUsuarios aoEnviar={proximo} />,
+    <DadosUsuarios aoEnviar={coletarDados} />,
     <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
-    <DadosEntregas aoEnviar={aoEnviar} />,
+    <DadosEntregas aoEnviar={coletarDados} />,
   ];
+
+  function coletarDados(dados) {
+    setDadosColetados({ ...dadosColetados, ...dados });
+    console.log(dadosColetados);
+    proximo();
+  }
 
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
