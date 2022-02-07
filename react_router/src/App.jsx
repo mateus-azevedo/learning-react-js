cfa74d8ec088f50420e6550d3b029b642cda6ffd
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-07 15:09:35 -0300

    exibindo posts por categoria

--- react_router/src/App.jsx
+++ react_router/src/App.jsx
@@ -6,6 +6,7 @@ import Sobre from "./paginas/Sobre";
 import Pagina404 from "./paginas/Pagina404";
 import Cabecalho from "./components/Cabecalho";
 import Post from "./paginas/Post";
+import Categoria from "./paginas/Categoria";
 
 function App() {
   return (
@@ -15,12 +16,19 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
+
         <Route path="/sobre">
           <Sobre />
         </Route>
+
+        <Route path="/categoria/:id">
+          <Categoria />
+        </Route>
+
         <Route path="/posts/:id">
           <Post />
         </Route>
+
         <Route>
           <Pagina404 />
         </Route>
