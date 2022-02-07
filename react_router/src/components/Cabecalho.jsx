42dfc0d9264527aef707792fffba16072b33bb4f
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-07 12:02:10 -0300

    utilizando o component Link

--- react_router/src/components/Cabecalho.jsx
+++ react_router/src/components/Cabecalho.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import imagem from "../assets/img/doguito.svg";
 import "../assets/css/componentes/cabecalho.css";
+import { Link } from "react-router-dom";
 
 const Cabecalho = () => {
   return (
@@ -9,27 +10,32 @@ const Cabecalho = () => {
         <span className="menu-hamburger__icone"></span>
       </div>
       <div assName="cabecalho-contaner">
-        <a href="/" className="flex flex--centro">
+        <Link to="/" className="flex flex--centro">
           <img className="cabecalho__logo" src={imagem} alt="Logo Doguito" />
           <h1 className="cabecalho__titulo">PetShop</h1>
-        </a>
+        </Link>
       </div>
       <nav className="menu-cabecalho">
         <ul className="menu-itens">
           <li>
-            <a href="#" className="menu-item menu-item--entrar">
+            <Link to="#" className="menu-item menu-item--entrar">
               Entrar
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="menu-item">
+            <Link to="/" className="menu-item">
+              Produtos
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="menu-item">
               Blog
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="menu-item">
+            <Link to="/sobre" className="menu-item">
               Sobre
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
