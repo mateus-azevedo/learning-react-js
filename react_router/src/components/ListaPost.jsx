39bbd85cd8e4495ebb350dc3c69065c157651f94
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-07 14:22:05 -0300

    exibindo a pagina de post

--- react_router/src/components/ListaPost.jsx
+++ react_router/src/components/ListaPost.jsx
@@ -7,12 +7,15 @@ const ListaPost = ({ url }) => {
 
   useEffect(() => {
     busca(url, setPosts);
-  }, []);
+  }, [url]);
 
   return (
     <section className="posts container">
       {posts.map((post) => (
-        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link
+          className={`cartao-post cartao-post--${post.categoria}`}
+          to={`/posts/${post.id}`}
+        >
           <article key={post.id}>
             <h3 className="cartao-post__titulo">{post.title}</h3>
             <p className="cartao-post__meta">{post.metadescription}</p>
