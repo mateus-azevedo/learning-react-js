62caf9181d0de646c383a025295bd23fc12c2995
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-07 13:39:21 -0300

    conexao com api

---
+++ react_router/src/components/ListaPOst.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { busca } from "../api/api";
+
+const ListaPost = () => {
+  return <section className="posts container"></section>;
+};
+
+export default ListaPost;
