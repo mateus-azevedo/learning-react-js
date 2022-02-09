b3644bb95850bd8224ea7a4c9352a1189d7ae365
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-09 10:07:08 -0300

    testando funcoes

--- react_testes_automatizados/src/App.test.js
+++ react_testes_automatizados/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import App from "./App";
+import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
   describe("Quando eu abro o app do banco", () => {
@@ -21,6 +21,28 @@ describe("Componente principal", () => {
       render(<App />);
 
       expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
+  });
+
+  describe("Quando eu realizo uma transação", () => {
+    it("de saque, o valor deveria diminuir", () => {
+      const valores = {
+        transacao: "saque",
+        valor: 50,
+      };
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+
+      expect(novoSaldo).toBe(100);
+    });
+
+    it("de deposito, o valor deveria aumentar", () => {
+      const valores = {
+        transacao: "deposito",
+        valor: 50,
+      };
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+
+      expect(novoSaldo).toBe(200);
     });
   });
 });
