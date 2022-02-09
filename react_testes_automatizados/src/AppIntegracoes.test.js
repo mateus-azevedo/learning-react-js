93325eaed242724e3dda4578daa79778a0444364
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-09 14:20:42 -0300

    simulando o retorno de uma API

---
+++ react_testes_automatizados/src/AppIntegracoes.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+import api from "./api";
+
+jest.mock("./api");
+
+describe("Requisições para API", () => {
+  it("Exibir lista de transações da API", () => {
+    api.listaTransacoes.mockResolvedValue([
+      {
+        id: 1,
+        valor: 10,
+        transacao: "saque",
+        data: "10/08/2020",
+      },
+      {
+        id: 2,
+        valor: 20,
+        transacao: "deposito",
+        data: "26/09/2020",
+      },
+    ]);
+
+    render(<App />);
+    // expectScreenOrientation.findByText("saque").toBeIn;
+    expect(screen.getByTestId("transacoes").children.length).toBe(2);
+  });
+});
