d8ab31b7ba310c7ba042f50e057f5a355dcebef5
Mateus Santos <m.santos.azeve@gmail.com>
2022-02-09 14:27:01 -0300

    testando codigo assincrono

--- react_testes_automatizados/src/AppIntegracoes.test.js
+++ react_testes_automatizados/src/AppIntegracoes.test.js
@@ -7,24 +7,26 @@ import api from "./api";
 jest.mock("./api");
 
 describe("Requisições para API", () => {
-  it("Exibir lista de transações da API", () => {
+  it("Exibir lista de transações da API", async () => {
     api.listaTransacoes.mockResolvedValue([
       {
         id: 1,
-        valor: 10,
+        valor: "10",
         transacao: "saque",
         data: "10/08/2020",
       },
       {
         id: 2,
-        valor: 20,
+        valor: "20",
         transacao: "deposito",
         data: "26/09/2020",
       },
     ]);
 
     render(<App />);
-    // expectScreenOrientation.findByText("saque").toBeIn;
+
+    expect(await screen.findByText("saque")).toBeInTheDocument();
+
     expect(screen.getByTestId("transacoes").children.length).toBe(2);
   });
 });
